cc47dfe7715214da9baddf9816fc20f409aff8a5
Andrew <andrew.g.93@hotmail.com>
2020-12-15 15:07:35 +1100

    Added code to main.py to create the flask app

---
+++ src/main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow.exceptions import ValidationError
+
+
+# Load env variables and initialise packages
+load_dotenv()
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+migrate = Migrate()
+
+
+def create_app():
+    """
+    Factory pattern used to create instances of a Flask app
+
+    Returns:
+    An instance of the Flask app
+    """
+
+    # These need to be inside the function
+    from src.controllers import registerable_controllers
+
+    # Create the app and load default config settings
+    app = Flask(__name__)
+    app.config.from_object("src.default_settings.app_config")
+
+    # Bind extensions to the app
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400)
+
+    return app
