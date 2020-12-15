399c349dd62e0555a9d64270a0b42fdec655beeb
Andrew <andrew.g.93@hotmail.com>
2020-12-15 16:34:40 +1100

    Created User model and uncommented parts of admin_controller.py

---
+++ src/models/User.py
@@ -0,0 +1,19 @@
+from src.main import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+    country = db.Column(db.String(), nullable=True)
+    display_name = db.Column(db.String(), nullable=True)
+    href = db.Column(db.String(), nullable=False)
+    product = db.Column(db.String(), nullable=False, default="free")
+    object_type = db.Column(db.String(), nullable=False, default="user")
+    uri = db.Column(db.String(), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
