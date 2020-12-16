24c5d3bb11e8aae52c409e8d199ab0205a010150
Andrew <andrew.g.93@hotmail.com>
2020-12-16 12:26:45 +1100

    Created Track model

---
+++ src/models/Track.py
@@ -0,0 +1,22 @@
+from src.main import db
+
+
+class Track(db.Model):
+    __tablename__ = "tracks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=False)
+    album = db.relationship("Album", back_populates="tracks")
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
+    artist = db.relationship("Artist", back_populates="tracks")
+    disc_number = db.Column(db.Integer)
+    duration_ms = db.Column(db.Integer, nullable=False)
+    explicit = db.Column(db.Boolean, nullable=False, default=False)
+    href = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=False)
+    popularity = db.Column(db.Integer, nullable=False, default=0)
+    preview_url = db.Column(db.String(), nullable=False)
+    track_number = db.Column(db.Integer, nullable=False)
+    object_type = db.Column(db.String(20), nullable=False, default="track")
+    uri = db.Column(db.String(), nullable=False)
+    is_local = db.Column(db.Boolean, nullable=False, default=False)
