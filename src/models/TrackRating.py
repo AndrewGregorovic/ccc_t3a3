2272e223f78bdb11093b0b5c3a1d1ed2104d6562
Andrew <andrew.g.93@hotmail.com>
2020-12-16 12:22:49 +1100

    Created TrackRatings model and updated User model

---
+++ src/models/TrackRating.py
@@ -0,0 +1,17 @@
+from src.main import db
+
+
+class Track_Ratings(db.Model):
+    __tablename__ = "track_ratings"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    track_id = db.Column(db.Integer, db.ForeignKey("tracks.id"), primary_key=True)
+    rating = db.Column(db.Integer, nullable=False, default=0)
+    track = db.relationship("Track")
+
+# track_ratings = db.Table("track_ratings", db.Model.metadata,
+#                          db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+#                          db.Column("track_id", db.Integer, db.ForeignKey("tracks.id"), primary_key=True),
+#                          db.Column("rating", db.Integer, nullable=False, default=0)
+#                          db.relationship("Track")
+#                          )
--- src/models/User.py
+++ src/models/User.py
@@ -1,4 +1,5 @@
 from src.main import db
+from src.models.TrackRating import track_ratings
 
 
 class User(db.Model):
@@ -7,12 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-    country = db.Column(db.String(), nullable=True)
-    display_name = db.Column(db.String(), nullable=True)
+    country = db.Column(db.String(2), nullable=True)
+    display_name = db.Column(db.String(30), nullable=True)
     href = db.Column(db.String(), nullable=False)
-    product = db.Column(db.String(), nullable=False, default="free")
-    object_type = db.Column(db.String(), nullable=False, default="user")
+    product = db.Column(db.String(20), nullable=False, default="free")
+    object_type = db.Column(db.String(20), nullable=False, default="user")
     uri = db.Column(db.String(), nullable=False)
+    track_ratings = db.relationship(track_ratings)
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
