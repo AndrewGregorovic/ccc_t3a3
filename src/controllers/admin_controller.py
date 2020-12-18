b0989b2ba4af29a11e3128a1466be902c4e91d9b
Andrew <andrew.g.93@hotmail.com>
2020-12-19 10:13:58 +1100

    Added admin endpoint that counts the number of users with each product type

--- src/controllers/admin_controller.py
+++ src/controllers/admin_controller.py
@@ -1,6 +1,7 @@
-from flask import abort, Blueprint
+from flask import abort, Blueprint, jsonify
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
+from src.main import db
 from src.models.Admin import Admin
 from src.models.User import User
 
@@ -23,3 +24,24 @@ def dump_database():
         return abort(401, description="Unauthorized.")
 
     return Admin.dump_database()
+
+
+@admin.route("/product_numbers", methods=["GET"])
+@jwt_required
+def get_product_numbers():
+    """
+    Endpoint only available for administrators to view the number of users with each product
+
+    Returns:
+    Dict of the product type and number of users for each product type
+    """
+
+    user = User.query.get(get_jwt_identity())
+    if not user.admin:
+        return abort(401, description="Unauthorized.")
+
+    product_numbers = User.query\
+        .with_entities(User.product, db.func.count(User.product))\
+        .group_by(User.product).all()
+
+    return jsonify([dict(zip(("product_type", "users"), product)) for product in product_numbers])
