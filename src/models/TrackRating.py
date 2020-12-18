7ec043b82b3545fa28e7e70fdaf1f3811b692421
Andrew <andrew.g.93@hotmail.com>
2020-12-18 12:26:36 +1100

    Removed default for rating, a zero rating should instead be removed from the table

--- src/models/TrackRating.py
+++ src/models/TrackRating.py
@@ -6,12 +6,5 @@ class Track_Rating(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
     track_id = db.Column(db.Integer, db.ForeignKey("tracks.id"), primary_key=True)
-    rating = db.Column(db.Integer, nullable=False, default=0)
+    rating = db.Column(db.Integer, nullable=False)
     track = db.relationship("Track")
-
-# track_ratings = db.Table("track_ratings", db.Model.metadata,
-#                          db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
-#                          db.Column("track_id", db.Integer, db.ForeignKey("tracks.id"), primary_key=True),
-#                          db.Column("rating", db.Integer, nullable=False, default=0)
-#                          db.relationship("Track")
-#                          )
